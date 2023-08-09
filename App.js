0221bc33e10b13f26e48256cbc8410362b311682
maheshuit <126758164+maheshuit@users.noreply.github.com>
2023-08-09 09:57:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from './demo1'
+
+function App() {
+  return (
+   <Demo></Demo>
+  );
+}
+
+export default App;
---
+++ demo1.js
@@ -0,0 +1,14 @@
+// function component
+import React from 'react';
+
+function Demo(){
+    return(<>
+        
+<h1>React app</h1>
+<h3> Create React App gives developers a React project structure, the possibility to use the latest 
+    JavaScript features and tools for optimizing your app for production.
+</h3>
+
+    </>)
+}
+export default Demo;
